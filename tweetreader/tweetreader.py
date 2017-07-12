8f56ad6a9869ef27f06af46779328df69e2ec392
Jose Ortiz <jlortiz@uc.cl>
2017-07-12 17:15:16 -0400

    Displays latest tweets using REST API through tweepy library (polling). Streaming was explored and tested but will be left for a future release.

--- tweetreader/tweetreader.py
+++ tweetreader/tweetreader.py
@@ -1,9 +1,12 @@
-
 from __future__ import print_function
 import os
 import sys
 import tweepy
+from requests.packages.urllib3 import exceptions
+import warnings
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, Markup
+
+warnings.filterwarnings(action='ignore', category=exceptions.InsecurePlatformWarning)
 
 app = Flask(__name__, instance_relative_config=True) # create the application instance :)
 app.config.from_object(__name__) # load config from this file
@@ -16,23 +19,41 @@ app.config.update(dict(
                        ACCESS_TOKEN        = 'Get from https://dev.twitter.com/',
                        ACCESS_TOKEN_SECRET = 'Get from https://dev.twitter.com/'  ,
                        SECRET_KEY = 'V0tISH0cy1pGyynHyfKI',
-                       TITLE = 'AMGEngTest robot tweet reader'
+                       TITLE = 'ALMA Engineering Test robot tweet reader',
+                       DEV_MODE = False
                       )
                  )
 app.config.from_pyfile('application.cfg', silent=True)
 
+def GetLatestTweets():
+    if not app.config['DEV_MODE']:
+        auth = tweepy.OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+        auth.set_access_token(app.config['ACCESS_TOKEN'], app.config['ACCESS_TOKEN_SECRET'])
+        api = tweepy.API(auth)
+
+        # Get latest 20 tweets by defined user
+        latest_tweets = api.home_timeline()
+    else:
+        print('DEV MODE')
+        import pickle
+        with open('test_tweets.pkl', 'r') as f:
+            latest_tweets = pickle.load(f)
+    return latest_tweets
+
 @app.route('/')
-def DisplayTweets():
-  auth = tweepy.OAuthHandler(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
-  auth.set_access_token(app.config['ACCESS_TOKEN'], app.config['ACCESS_TOKEN_SECRET'])
-  api = tweepy.API(auth)
+def DisplayMainPage():
+    # Get latest 20 tweets by defined user
+    latest_tweets = GetLatestTweets()
 
-  public_tweets = api.home_timeline()
-  # for tweet in public_tweets:
-  #     print(tweet.text)
-  print(public_tweets[0])
-  return render_template('show_tweets.html', tweets=public_tweets)
+    return render_template('show_tweets.html', tweets=latest_tweets, include_layout=True)
 
-# @app.errorhandler(404)
-# def pageNotFound(error):
-#     return render_template('page_not_found.html'), 404
+@app.route('/latest')
+def LatestTweets():
+    # Get latest 20 tweets by defined user
+    latest_tweets = GetLatestTweets()
+
+    return render_template('show_tweets.html', tweets=latest_tweets, include_layout=False)
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page_not_found.html'), 404
